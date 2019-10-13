07cd506dc095bc70e1467b2b31d41ca36914484f
Kimsj0616 <55050852+Kimsj0616@users.noreply.github.com>
2019-10-13 17:54:51 +0900

    kimkim

--- Termproject/app/src/main/java/com/kimsj/Termproject/MainActivity.kt
+++ Termproject/app/src/main/java/com/kimsj/Termproject/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.integration.android.IntentIntegrator
 import android.R.attr.data
 import android.widget.Toast
@@ -18,9 +20,17 @@ class MainActivity : AppCompatActivity() {
     private var createQRBtn: Button? = null
     private var scanQRBtn: Button? = null
 
+    private var firebasedb : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private  var ref : DatabaseReference = firebasedb.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this@MainActivity,"asd",Toast.LENGTH_LONG).show()
+        ref.setValue("김세종")
+        ref.child("name").setValue("김세종")
+
 
         createQRBtn = findViewById(R.id.createQR)
         scanQRBtn = findViewById(R.id.scanQR)
