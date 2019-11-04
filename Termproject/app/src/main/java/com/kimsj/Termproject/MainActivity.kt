c5948355843fdeb2e420ee147f4e5b00ab509840
hanboy6010 <40016761+hanboy6010@users.noreply.github.com>
2019-11-04 17:54:36 +0900

    GaGa

--- Termproject/app/src/main/java/com/kimsj/Termproject/MainActivity.kt
+++ Termproject/app/src/main/java/com/kimsj/Termproject/MainActivity.kt
@@ -1,27 +1,19 @@
 package com.kimsj.Termproject
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.zxing.integration.android.IntentIntegrator
-import android.R.attr.data
-import android.widget.ImageButton
-import android.widget.Toast
-import com.google.zxing.integration.android.IntentResult
-
 
 
 class MainActivity : AppCompatActivity() {
 
     private var scanQRBtn: ImageButton? = null
 
-    private var firebasedb : FirebaseDatabase = FirebaseDatabase.getInstance()
-    private  var ref : DatabaseReference = firebasedb.reference
+    private var firebasedb: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var ref: DatabaseReference = firebasedb.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         scanQRBtn = findViewById(R.id.scanQR)
 
-        scanQRBtn!!.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                val intent : Intent = Intent(this@MainActivity,ScanQR::class.java)
+        scanQRBtn!!.setOnClickListener {
+            val intent = Intent(this, ScanQR::class.java)
 
-                startActivity(intent)
-            }
-        })
+            startActivity(intent)
+        }
     }
 }
+
--- Termproject/app/src/main/java/com/kimsj/Termproject/Order.kt
+++ Termproject/app/src/main/java/com/kimsj/Termproject/Order.kt
@@ -1,13 +1,19 @@
 package com.kimsj.Termproject
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_order.*
 
 class Order : AppCompatActivity()
 {
-    private var OrderButton: Button? = null
+    var qttmenu1 = 0
+    var qttmenu2 = 0
+    var qttmenu3 = 0
+    var qttmenu4 = 0
+    var qttmenu5 = 0
+    var total_price = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +28,104 @@ class Order : AppCompatActivity()
             tableNumber.text = tableNo.toString()
         }
 
+        plusmenu1.setOnClickListener {
+            qttmenu1++
+            total_price += menu1price.text.toString().toInt()
+            totalprice.text = total_price.toString()
+            menu1qtt.text = qttmenu1.toString()
+        }
 
-        OrderButton = findViewById(R.id.order_button)
+        plusmenu2.setOnClickListener {
+            qttmenu2++
+            total_price += menu2price.text.toString().toInt()
+            totalprice.text = total_price.toString()
+            menu2qtt.text = qttmenu2.toString()
+        }
 
+        plusmenu3.setOnClickListener {
+            qttmenu3++
+            total_price += menu3price.text.toString().toInt()
+            totalprice.text = total_price.toString()
+            menu3qtt.text = qttmenu3.toString()
+        }
+
+        plusmenu4.setOnClickListener {
+            qttmenu4++
+            total_price += menu4price.text.toString().toInt()
+            totalprice.text = total_price.toString()
+            menu4qtt.text = qttmenu4.toString()
+        }
+
+        plusmenu5.setOnClickListener {
+            qttmenu5++
+            total_price += menu5price.text.toString().toInt()
+            totalprice.text = total_price.toString()
+            menu5qtt.text = qttmenu5.toString()
+        }
+
+        minusmenu1.setOnClickListener {
+            if(qttmenu1 == 0){
+                Toast.makeText(this@Order, "상품을 0개 이하로 주문하실 수 없습니다!!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                qttmenu1--
+                total_price -= menu1price.text.toString().toInt()
+                totalprice.text = total_price.toString()
+                menu1qtt.text = qttmenu1.toString()
+            }
+        }
+
+        minusmenu2.setOnClickListener {
+            if(qttmenu2 == 0){
+                Toast.makeText(this@Order, "상품을 0개 이하로 주문하실 수 없습니다!!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                qttmenu2--
+                total_price -= menu2price.text.toString().toInt()
+                totalprice.text = total_price.toString()
+                menu2qtt.text = qttmenu2.toString()
+            }
+        }
+
+        minusmenu3.setOnClickListener {
+            if(qttmenu3 == 0){
+                Toast.makeText(this@Order, "상품을 0개 이하로 주문하실 수 없습니다!!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                qttmenu3--
+                total_price -= menu3price.text.toString().toInt()
+                totalprice.text = total_price.toString()
+                menu3qtt.text = qttmenu3.toString()
+            }
+        }
+
+        minusmenu4.setOnClickListener {
+            if(qttmenu4 == 0){
+                Toast.makeText(this@Order, "상품을 0개 이하로 주문하실 수 없습니다!!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                qttmenu4--
+                total_price -= menu4price.text.toString().toInt()
+                totalprice.text = total_price.toString()
+                menu4qtt.text = qttmenu4.toString()
+            }
+        }
+
+        minusmenu5.setOnClickListener {
+            if(qttmenu5 == 0){
+                Toast.makeText(this@Order, "상품을 0개 이하로 주문하실 수 없습니다!!", Toast.LENGTH_LONG).show()
+            }
+            else{
+                qttmenu5--
+                total_price -= menu5price.text.toString().toInt()
+                totalprice.text = total_price.toString()
+                menu5qtt.text = qttmenu5.toString()
+            }
+        }
+
+        order_button.setOnClickListener {
+
+        }
     }
+
 }
