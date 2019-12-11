3bda7e9db1a4423765d2d6fbd2a30bdee3e4de66
Kimsj0616 <55050852+Kimsj0616@users.noreply.github.com>
2019-12-11 19:14:23 +0900

    Update OrderDB.kt

--- Termproject/app/src/main/java/com/kimsj/Termproject/OrderDB.kt
+++ Termproject/app/src/main/java/com/kimsj/Termproject/OrderDB.kt
@@ -1,63 +1,22 @@
 package com.kimsj.Termproject
 
-import android.os.Bundle
-
-import androidx.appcompat.app.AppCompatActivity
-
-import com.google.firebase.database.*
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 
-
+@Parcelize
 class OrderDB(var tablenumber: Int?,
               var qttmenu1:Int?,
               var qttmenu2:Int?,
               var qttmenu3:Int?,
               var qttmenu4:Int?,
-              var qttmenu5:Int?) : AppCompatActivity(){
+              var qttmenu5:Int?) : Parcelable {
 
-    var menu1 : String ? = null
-    var menu2 : String ? = null
-    var menu3 : String ? = null
-    var menu4 : String ? = null
-    var menu5 : String ? = null
-
-    var map1 = mapOf("Table number" to tablenumber,
-        menu1 to qttmenu1,
-        menu2 to qttmenu2,
-        menu3 to qttmenu3,
-        menu4 to qttmenu4,
-        menu5 to qttmenu5)
-
-    private var firebasedb : FirebaseDatabase = FirebaseDatabase.getInstance()
-    private var ref : DatabaseReference = firebasedb.reference
-    var menus : ArrayList<String> = ArrayList()
-    var prices : ArrayList<Int> = ArrayList()
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-
-            }
-            override fun onDataChange(p0: DataSnapshot) {
-
-                for(i in p0.child("menulist").children)
-                {
-                    menus.add(i.key.toString())
-                    prices.add(i.getValue(true).toString().toInt())
-                }
-
-                menu1 = menus[0]
-                menu2 = menus[1]
-                menu3 = menus[2]
-                menu4 = menus[3]
-                menu5 = menus[4]
-
-            }
-        })
-    }
-
+    val map1 = mapOf("Table number" to tablenumber,
+        "고구마치즈돈까스" to qttmenu1,
+        "돈까스" to qttmenu2,
+        "스파게티" to qttmenu3,
+        "치즈돈까스" to qttmenu4,
+        "콜라" to qttmenu5)
 }
